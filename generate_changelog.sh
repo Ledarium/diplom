0f7aab62e26972bcb3b3faa952a39ae05f7f0faa
Dmitry Maylarov <maylarov@yandex.ru>
2020-04-25 21:16:53 +0300

    Генератор changelog

---
+++ generate_changelog.sh
@@ -0,0 +1,9 @@
+alias gl="git --no-pager log --pretty=format:'%s (%h)'"
+echo "### Плюсы\n"
+gl $1...$2 --grep='+ ' --reverse
+echo "\n\n### Минусы\n"
+gl $1...$2 --grep='- ' --reverse
+echo "\n\n### Подводные камни\n"
+gl $1...$2 --grep='+ ' --grep='- ' --invert-grep --reverse
+echo "\n"
+
