1a14313b46387471fd056a5e5002862679f71aae
Dmitry Maylarov <maylarov@yandex.ru>
2020-05-29 20:26:35 +0300

    + Титульный в конце

--- slides/make_slides.sh
+++ slides/make_slides.sh
@@ -1,1 +1,3 @@
-pandoc -t beamer -V theme:metropolis --pdf-engine=xelatex slides.md -o slides.pdf --template beamer.tex
+pandoc -t beamer -V theme:metropolis --pdf-engine=xelatex slides.md -o out.pdf --template beamer.tex
+
+pdftk out.pdf cat 1-end 1 output slides.pdf
